9f678a726c976e5dd470be439230e2f4aca479d5
Grant Ogden <grant101.ogden@gmail.com>
2021-12-11 12:03:07 -0600

    Refactor to store values and then just use the stored values

--- Day1/cmd/DepthSlideInc.go
+++ Day1/cmd/DepthSlideInc.go
@@ -18,78 +18,33 @@ func main() {
 	argsWithoutProg := os.Args[1:]
 	counter := 0
 	curNum := 0
+	dataSlice := make([]int, 0, 0)
 	prevTotal := math.MaxInt
 	curTotal := 0
-	i := 4
-	scanA := [3]int{0, 0, 0}
-	scanB := [3]int{0, 0, 0}
-	scanC := [3]int{0, 0, 0}
-	scanD := [3]int{0, 0, 0}
 	filePath := argsWithoutProg[0]
 
+	// Open the file given as an argument
 	file, err := os.Open(filePath)
 	check(err)
 	defer file.Close()
 
+	// Create a scanner to read the file
 	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	scanA[0], err = strconv.Atoi(scanner.Text())
-	check(err)
-	scanA[1], err = strconv.Atoi(scanner.Text())
-	check(err)
-	scanA[2], err = strconv.Atoi(scanner.Text())
-	check(err)
 
-	scanB[0] = scanA[1]
-	scanB[1] = scanA[2]
-
-	scanC[0] = scanA[2]
-
+	// Scan the Values and convert them to integers and store in a slice
 	for scanner.Scan() {
 		curNum, err = strconv.Atoi(scanner.Text())
 		check(err)
-		switch i % 4 {
-		case 0:
-			scanB[2] = curNum
-			scanC[1] = curNum
-			scanD[0] = curNum
+		dataSlice = append(dataSlice, curNum)
+	}
 
-			curTotal = scanA[0] + scanA[1] + scanA[2]
-
-		case 1:
-			scanA[0] = curNum
-			scanC[2] = curNum
-			scanD[1] = curNum
-
-			curTotal = scanB[0] + scanB[1] + scanB[2]
-
-		case 2:
-			scanA[1] = curNum
-			scanB[0] = curNum
-			scanD[2] = curNum
-
-			curTotal = scanC[0] + scanC[1] + scanC[2]
-
-		case 3:
-			scanA[2] = curNum
-			scanB[1] = curNum
-			scanC[0] = curNum
-
-			curTotal = scanD[0] + scanD[1] + scanD[2]
-		}
-
-		i++
-		/* Debug code
-		fmt.Printf("PrevTotal: %d\n", prevTotal)
-		fmt.Printf("CurrTotal: %d\n", curTotal)
-		fmt.Printf("Counter  : %d\n", counter)
-
-		fmt.Scanln()*/
-
+	for i := 0; i < len(dataSlice)-2; i++ {
+		curTotal = dataSlice[i] + dataSlice[i+1] + dataSlice[i+2]
 		if curTotal > prevTotal {
 			counter++
 		}
 		prevTotal = curTotal
 	}
+
 	fmt.Printf("Number of Increases: %d\n", counter)
 }
